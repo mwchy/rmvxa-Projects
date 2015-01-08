dc60c6567ce40d29fcabc4ec3ef750d9825ad1bb
mwchy <mwchycraft@gmail.com>
2015-01-07 19:03:34 -0800

    Update fontresize.rb

--- fontresize.rb
+++ fontresize.rb
@@ -2,6 +2,6 @@
 ##resizes to 640x480
 ##goes in main, before rgss_main call
 
-Font.default_name = ["Perfect DOS VGA 437"] 
+Font.default_name = [" "] 
 Font.default_size = 22 
 Graphics.resize_screen(640, 480)
