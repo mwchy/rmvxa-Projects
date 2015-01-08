8610ee5ad8ea991d64d1739a01e9d0cabf418ea2
mwchy <mwchycraft@gmail.com>
2015-01-07 19:03:24 -0800

    Update and rename 640x480.rb to fontresize.rb

--- 640x480.rb
+++
@@ -1,4 +0,0 @@
-##resizes to 640x480
-##goes in main, before rgss_main call
-
-Graphics.resize_screen(640, 480)
---
+++ fontresize.rb
@@ -0,0 +1,7 @@
+##changes all in game fonts
+##resizes to 640x480
+##goes in main, before rgss_main call
+
+Font.default_name = ["Perfect DOS VGA 437"] 
+Font.default_size = 22 
+Graphics.resize_screen(640, 480)
